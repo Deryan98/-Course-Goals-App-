f763155cb06653aacf2ab9c04f0e98e686b7ed23
Didier Cruz <didiercruz@MacBook-Pro-de-Didier.local>
2020-07-29 00:42:52 -0600

    More Flexbox Styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -27,8 +27,8 @@ const GoalInput = (props) => {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   input: {
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10,
+    marginBottom: 10,
   },
 });
 
